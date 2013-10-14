8ec6503edd7e75fb1501e9bcd266e811f2ce3538
James Page <james.page@canonical.com>
2013-10-14 15:58:16 +0100

    Rebase on charmhelpers

--- hooks/ceilometer_utils.py
+++ hooks/ceilometer_utils.py
@@ -2,22 +2,29 @@ import os
 import uuid
 import ConfigParser
 import sys
-from lib.utils import juju_log
+from charmhelpers.fetch import apt_install as install
+from charmhelpers.core.hookenv import log as juju_log, ERROR
 
 RABBIT_USER = "ceilometer"
 RABBIT_VHOST = "ceilometer"
 CEILOMETER_CONF = "/etc/ceilometer/ceilometer.conf"
 
 SHARED_SECRET = "/etc/ceilometer/secret.txt"
-CEILOMETER_SERVICES = ['ceilometer-agent-central', 'ceilometer-collector',
-    'ceilometer-api']
+CEILOMETER_SERVICES = [
+    'ceilometer-agent-central', 'ceilometer-collector',
+    'ceilometer-api'
+]
 CEILOMETER_DB = "ceilometer"
 CEILOMETER_SERVICE = "ceilometer"
 CEILOMETER_COMPUTE_SERVICES = ['ceilometer-agent-compute']
-CEILOMETER_PACKAGES = ['python-ceilometer', 'ceilometer-common',
-    'ceilometer-agent-central', 'ceilometer-collector', 'ceilometer-api']
-CEILOMETER_AGENT_PACKAGES = ['python-ceilometer', 'ceilometer-common',
-    'ceilometer-agent-compute']
+CEILOMETER_PACKAGES = [
+    'python-ceilometer', 'ceilometer-common',
+    'ceilometer-agent-central', 'ceilometer-collector', 'ceilometer-api'
+]
+CEILOMETER_AGENT_PACKAGES = [
+    'python-ceilometer', 'ceilometer-common',
+    'ceilometer-agent-compute'
+]
 CEILOMETER_PORT = 8777
 CEILOMETER_ROLE = "ResellerAdmin"
 
@@ -25,7 +32,8 @@ NOVA_CONF = "/etc/nova/nova.conf"
 NOVA_SETTINGS = [
     ('DEFAULT', 'instance_usage_audit', 'True'),
     ('DEFAULT', 'instance_usage_audit_period', 'hour'),
-    ('DEFAULT', 'notification_driver', 'ceilometer.compute.nova_notifier')]
+    ('DEFAULT', 'notification_driver', 'ceilometer.compute.nova_notifier')
+]
 
 
 def get_shared_secret():
@@ -60,5 +68,21 @@ def modify_config_file(nova_conf, values):
 
         f.close()
     except IOError:
-        juju_log('ERROR', 'Error updating nova config file')
+        juju_log('Error updating nova config file', level=ERROR)
         sys.exit(1)
+
+
+TEMPLATES_DIR = 'templates'
+
+try:
+    import jinja2
+except ImportError:
+    install(['python-jinja2'])
+    import jinja2
+
+
+def render_template(template_name, context, template_dir=TEMPLATES_DIR):
+    templates = \
+        jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+    template = templates.get_template(template_name)
+    return template.render(context)
--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,100 +1,131 @@
 #!/usr/bin/python
 
 import os
-import lib.utils as utils
-import ceilometer_utils
+import sys
+#import lib.utils as utils
+from charmhelpers.fetch import (
+    apt_install, filter_installed_packages,
+)
+from charmhelpers.core.hookenv import (
+    open_port,
+    relation_set,
+    relation_get,
+    relation_ids,
+    related_units,
+    config,
+    unit_get,
+    Hooks, UnregisteredHookError,
+    log
+)
+from charmhelpers.core.host import (
+    service_restart,
+)
+from charmhelpers.contrib.openstack.utils import (
+    configure_installation_source
+)
+from ceilometer_utils import (
+    CEILOMETER_PACKAGES,
+    CEILOMETER_PORT,
+    RABBIT_USER,
+    RABBIT_VHOST,
+    CEILOMETER_DB,
+    CEILOMETER_SERVICES,
+    CEILOMETER_SERVICE,
+    CEILOMETER_ROLE,
+    CEILOMETER_CONF,
+    get_shared_secret,
+    render_template
+)
+
+hooks = Hooks()
 
 
+@hooks.hook()
 def install():
-    utils.configure_source()
-    utils.install(*ceilometer_utils.CEILOMETER_PACKAGES)
-    utils.expose(ceilometer_utils.CEILOMETER_PORT)
+    configure_installation_source(config('openstack-origin'))
+    apt_install(filter_installed_packages(CEILOMETER_PACKAGES),
+                fatal=True)
+    open_port(CEILOMETER_PORT)
 
 
+@hooks.hook("amqp-relation-joined")
 def amqp_joined():
-    utils.relation_set(username=ceilometer_utils.RABBIT_USER,
-        vhost=ceilometer_utils.RABBIT_VHOST)
+    relation_set(username=RABBIT_USER,
+                 vhost=RABBIT_VHOST)
 
 
-def amqp_changed():
-    if render_ceilometer_conf():
-        utils.restart(*ceilometer_utils.CEILOMETER_SERVICES)
-        ceilometer_joined()
-
-
+@hooks.hook("shared-db-relation-joined")
 def db_joined():
-    utils.relation_set(ceilometer_database=ceilometer_utils.CEILOMETER_DB)
+    relation_set(ceilometer_database=CEILOMETER_DB)
 
 
-def db_changed():
+@hooks.hook("amqp-relation-changed",
+            "shared-db-relation-changed",
+            "identity-service-relation-changed")
+def all_changed():
     if render_ceilometer_conf():
-        utils.restart(*ceilometer_utils.CEILOMETER_SERVICES)
+        for svc in CEILOMETER_SERVICES:
+            service_restart(svc)
         ceilometer_joined()
 
 
+@hooks.hook("identity-service-relation-joined")
 def keystone_joined():
-    port = ceilometer_utils.CEILOMETER_PORT
-    url = "http://" + utils.unit_get("private-address") + ":" + str(port)
-    region = utils.config_get("region")
+    url = "http://{}:{}".format(unit_get("private-address"),
+                                CEILOMETER_PORT)
+    region = config("region")
 
-    utils.relation_set(service=ceilometer_utils.CEILOMETER_SERVICE,
-        public_url=url, admin_url=url, internal_url=url,
-        requested_roles=ceilometer_utils.CEILOMETER_ROLE,
-        region=region)
-
-
-def keystone_changed():
-    if render_ceilometer_conf():
-        utils.restart(*ceilometer_utils.CEILOMETER_SERVICES)
-        ceilometer_joined()
+    relation_set(service=CEILOMETER_SERVICE,
+                 public_url=url, admin_url=url, internal_url=url,
+                 requested_roles=CEILOMETER_ROLE,
+                 region=region)
 
 
 def get_rabbit_conf():
-    for relid in utils.relation_ids('amqp'):
-        for unit in utils.relation_list(relid):
+    for relid in relation_ids('amqp'):
+        for unit in related_units(relid):
             conf = {
-                "rabbit_host": utils.relation_get('private-address',
-                                                  unit, relid),
-                "rabbit_virtual_host": ceilometer_utils.RABBIT_VHOST,
-                "rabbit_userid": ceilometer_utils.RABBIT_USER,
-                "rabbit_password": utils.relation_get('password',
-                                                      unit, relid)
-                }
-            if utils.relation_get('clustered',
-                                  unit, relid):
-                conf["rabbit_host"] = utils.relation_get('vip',
-                                                         unit, relid)
+                "rabbit_host": relation_get('private-address',
+                                            unit, relid),
+                "rabbit_virtual_host": RABBIT_VHOST,
+                "rabbit_userid": RABBIT_USER,
+                "rabbit_password": relation_get('password',
+                                                unit, relid)
+            }
+            if relation_get('clustered',
+                            unit, relid):
+                conf["rabbit_host"] = relation_get('vip', unit, relid)
             if None not in conf.itervalues():
                 return conf
     return None
 
 
 def get_db_conf():
-    for relid in utils.relation_ids('shared-db'):
-        for unit in utils.relation_list(relid):
+    for relid in relation_ids('shared-db'):
+        for unit in related_units(relid):
             conf = {
-                "db_host": utils.relation_get('hostname', unit, relid),
-                "db_port": utils.relation_get('port', unit, relid),
-                "db_name": ceilometer_utils.CEILOMETER_DB
-                }
+                "db_host": relation_get('hostname', unit, relid),
+                "db_port": relation_get('port', unit, relid),
+                "db_name": CEILOMETER_DB
+            }
             if None not in conf.itervalues():
                 return conf
     return None
 
 
 def get_keystone_conf():
-    for relid in utils.relation_ids('identity-service'):
-        for unit in utils.relation_list(relid):
-            keystone_username = utils.relation_get('service_username',
-                                                   unit, relid)
-            keystone_port = utils.relation_get('service_port',
-                                               unit, relid)
-            keystone_host = utils.relation_get('service_host',
-                                               unit, relid)
-            keystone_password = utils.relation_get('service_password',
-                                                   unit, relid)
-            keystone_tenant = utils.relation_get('service_tenant',
-                                                 unit, relid)
+    for relid in relation_ids('identity-service'):
+        for unit in related_units(relid):
+            keystone_username = relation_get('service_username',
+                                             unit, relid)
+            keystone_port = relation_get('service_port',
+                                         unit, relid)
+            keystone_host = relation_get('service_host',
+                                         unit, relid)
+            keystone_password = relation_get('service_password',
+                                             unit, relid)
+            keystone_tenant = relation_get('service_tenant',
+                                           unit, relid)
 
             conf = {
                 "keystone_os_username": keystone_username,
@@ -114,21 +145,22 @@ def render_ceilometer_conf():
     contextkeystone = get_keystone_conf()
 
     if (context and contextdb and contextkeystone and
-        os.path.exists(ceilometer_utils.CEILOMETER_CONF)):
+            os.path.exists(CEILOMETER_CONF)):
         # merge contexts
         context.update(contextkeystone)
         context.update(contextdb)
-        context['metering_secret'] = ceilometer_utils.get_shared_secret()
-        context['service_port'] = ceilometer_utils.CEILOMETER_PORT
+        context['metering_secret'] = get_shared_secret()
+        context['service_port'] = CEILOMETER_PORT
 
-        with open(ceilometer_utils.CEILOMETER_CONF, "w") as conf:
-            conf.write(utils.render_template(
-                os.path.basename(ceilometer_utils.CEILOMETER_CONF), context))
+        with open(CEILOMETER_CONF, "w") as conf:
+            conf.write(render_template(os.path.basename(CEILOMETER_CONF),
+                                       context))
 
         return True
     return False
 
 
+@hooks.hook("ceilometer-service-relation-joined")
 def ceilometer_joined():
     # set all relationships for ceilometer service
     context = get_rabbit_conf()
@@ -137,20 +169,13 @@ def ceilometer_joined():
     if context and contextdb and contextkeystone:
         context.update(contextdb)
         context.update(contextkeystone)
-        context["metering_secret"] = ceilometer_utils.get_shared_secret()
+        context["metering_secret"] = get_shared_secret()
         # set all that info into ceilometer-service relationship
-        for relid in utils.relation_ids('ceilometer-service'):
-            context["rid"] = relid
-            utils.relation_set(**context)
+        for relid in relation_ids('ceilometer-service'):
+            relation_set(relid, context)
 
-
-utils.do_hooks({
-    "install": install,
-    "amqp-relation-joined": amqp_joined,
-    "amqp-relation-changed": amqp_changed,
-    "shared-db-relation-joined": db_joined,
-    "shared-db-relation-changed": db_changed,
-    "identity-service-relation-joined": keystone_joined,
-    "identity-service-relation-changed": keystone_changed,
-    "ceilometer-service-relation-joined": ceilometer_joined
-})
+if __name__ == '__main__':
+    try:
+        hooks.execute(sys.argv)
+    except UnregisteredHookError as e:
+        log('Unknown hook {} - skipping.'.format(e))
