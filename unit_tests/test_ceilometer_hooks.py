505e021731bee981a3ef363ab7252496c351ad5b
Liam Young <liam.young@canonical.com>
2014-10-21 08:51:01 +0100

    Fix unit tests

--- unit_tests/test_ceilometer_hooks.py
+++ unit_tests/test_ceilometer_hooks.py
@@ -25,7 +25,6 @@ TO_PATCH = [
     'relation_ids',
     'filter_installed_packages',
     'CONFIGS',
-    'unit_get',
     'get_ceilometer_context',
     'lsb_release',
     'get_packages',
@@ -37,6 +36,7 @@ class CeilometerHooksTest(CharmTestCase):
 
     def setUp(self):
         super(CeilometerHooksTest, self).setUp(hooks, TO_PATCH)
+        hooks.hooks._config_save = False
         self.config.side_effect = self.test_config.get
         self.get_packages.return_value = ceilometer_utils.CEILOMETER_PACKAGES
         self.filter_installed_packages.return_value = \
@@ -117,7 +117,6 @@ class CeilometerHooksTest(CharmTestCase):
         self.assertTrue(joined.called)
 
     def test_keystone_joined(self):
-        self.unit_get.return_value = 'thishost'
         self.canonical_url.return_value = "http://thishost"
         self.test_config.set('region', 'myregion')
         hooks.hooks.execute(['hooks/identity-service-relation-joined'])
