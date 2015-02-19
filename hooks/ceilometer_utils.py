1c7fefe7e15b52b714356492a04d992a3dcc3b9b
Edward Hope-Morley <edward.hope-morley@canonical.com>
2015-02-19 20:07:47 +0000

    [hopem,r=]
    Adds pki token support.
    
    Closes-Bug: 1309667

--- hooks/ceilometer_utils.py
+++ hooks/ceilometer_utils.py
@@ -65,11 +65,12 @@ ICEHOUSE_SERVICES = [
 ]
 
 CEILOMETER_ROLE = "ResellerAdmin"
-
+SVC = 'ceilometer'
 
 CONFIG_FILES = OrderedDict([
     (CEILOMETER_CONF, {
-        'hook_contexts': [context.IdentityServiceContext(),
+        'hook_contexts': [context.IdentityServiceContext(service=SVC,
+                                                         service_user=SVC),
                           context.AMQPContext(ssl_dir=CEILOMETER_CONF_DIR),
                           LoggingConfigContext(),
                           MongoDBContext(),
