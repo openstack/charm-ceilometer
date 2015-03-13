f2cd084d41e5852926957316178ccd71da3e6520
Billy Olsen <billy.olsen@canonical.com>
2015-03-13 08:53:30 -0700

    ch sync

--- hooks/charmhelpers/core/hookenv.py
+++ hooks/charmhelpers/core/hookenv.py
@@ -352,6 +352,17 @@ def relation_set(relation_id=None, relation_settings=None, **kwargs):
     flush(local_unit())
 
 
+def relation_clear(r_id=None):
+    ''' Clears any relation data already set on relation r_id '''
+    settings = relation_get(rid=r_id,
+                            unit=local_unit())
+    for setting in settings:
+        if setting not in ['public-address', 'private-address']:
+            settings[setting] = None
+    relation_set(relation_id=r_id,
+                 **settings)
+
+
 @cached
 def relation_ids(reltype=None):
     """A list of relation_ids"""
@@ -581,7 +592,7 @@ def translate_exc(from_exc, to_exc):
     return inner_translate_exc1
 
 
-@translate_exc(from_exc=CalledProcessError, to_exc=NotImplementedError)
+@translate_exc(from_exc=OSError, to_exc=NotImplementedError)
 def is_leader():
     """Does the current unit hold the juju leadership
 
@@ -596,7 +607,7 @@ def is_leader():
         raise NotImplementedError
 
 
-@translate_exc(from_exc=CalledProcessError, to_exc=NotImplementedError)
+@translate_exc(from_exc=OSError, to_exc=NotImplementedError)
 def leader_get(attribute=None):
     """Juju leader get value(s)"""
     cmd = ['leader-get', '--format=json'] + [attribute or '-']
@@ -613,7 +624,7 @@ def leader_get(attribute=None):
         raise
 
 
-@translate_exc(from_exc=CalledProcessError, to_exc=NotImplementedError)
+@translate_exc(from_exc=OSError, to_exc=NotImplementedError)
 def leader_set(settings=None, **kwargs):
     """Juju leader set value(s)"""
     log("Juju leader-set '%s'" % (settings), level=DEBUG)
